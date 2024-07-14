8c28797601cc9a33321c30983abc896039f0b68d
Mambaul Hisam <hisyamsquartz@gmail.com>
2024-07-14 20:39:03 +0700

    input handling

--- app.py
+++ app.py
@@ -22,26 +22,63 @@ def detect(umur: int, jenis_kelamin: int, tinggi_badan: int) -> Tuple[str, int] 
     
     return None
 
+def wrap_jk(content: str | int) -> int:
+    if type(content) != str:
+        try:
+            return int(content)
+        except:
+            raise Exception('invalid value')
+
+    if content == 'laki-laki':
+        return 1
+    return 0
+
+def val_umur(content: str) -> int:
+    try:
+        umur = int(content)
+        if umur <= 60 and umur >= 0:
+            return umur
+        raise Exception('invalid months')
+    except:
+        raise Exception('invalid value')
+
+def wrap_tb(content: str | int) -> int:
+    if type(content) == str:
+        try:
+            return int(content)
+        except:
+            raise Exception('invalid value')
+    return int(content)
+
+
 @app.route('/', methods=['POST'])
 def predict():
     resp_obj: Dict[str, str | int] = {}
+
+    if request.headers.get('Content-Type') != 'application/json':
+        resp_obj['result'] = -1
+        resp_obj['message'] = 'mismatch content-type'
+        return jsonify(resp_obj), 400
+
     try:
-        content = request.json
+        content = request.get_json()
 
         if content is None:
-            raise Exception("request body are None")
+            resp_obj['result'] = -1
+            resp_obj['message'] = 'missing request body'
+            return jsonify(resp_obj), 400
     except:
         resp_obj['result'] = -1
         resp_obj['message'] = 'missing request body'
         return jsonify(resp_obj), 400
 
     try:
-        umur = int(content['umur'])
-        jk = int(content['jenis_kelamin'])
-        tb = int(content['tinggi_badan'])
-    except:
+        umur = val_umur(content['umur'])
+        jk = wrap_jk(content['jenis_kelamin'])
+        tb = wrap_tb(content['tinggi_badan'])
+    except Exception as ex:
         resp_obj['result'] = -1
-        resp_obj['message'] = 'missing json field'
+        resp_obj['message'] = ex.args[0]
         return jsonify(resp_obj), 400
 
     pred = detect(umur, jk, tb)
